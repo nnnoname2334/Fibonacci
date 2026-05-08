11865720a9f27e6c3e2660ae7d47a7cdf2e3e17d
234989 <234989@KB37-120-C10>
2026-05-08 12:11:49 +0300

    добавление комментария

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 
-class Program
+namespace Fibonacci
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        int result = Fibonacci(5);
-        Console.WriteLine(result);
-    }
-
-    static int Fibonacci(int n)
-    {
-        Console.WriteLine("The output is: ");
-        int n1 = 0;
-        int n2 = 1;
-        int sum;
-
-        for (int i = 2; i < n; i++)
+        static void Main(string[] args)
         {
-            sum = n1 + n2;
-            n1 = n2;
-            n2 = sum;
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
         }
 
-        return n == 0 ? n1 : n2;
+        /// <summary>
+        /// Вычисляет n-е число последовательности Фибоначчи
+        /// </summary>
+        /// <param name="n">Порядковый номер числа Фибоначчи</param>
+        /// <returns>n-е число Фибоначчи</returns>
+        static int Fibonacci(int n)
+        {
+            Console.WriteLine("The output is: ");
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
+        }
     }
 }
