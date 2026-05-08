4b4c48dfe955dacff25eff9faa8196faec3df253
234989 <234989@KB37-120-C10>
2026-05-08 13:16:51 +0300

    code

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,27 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class Program
+namespace practice7
 {
-    static void Main(string[] args)
+    class Program
     {
-        int result = Fibonacci(5);
-        Console.WriteLine(result);
-    }
-
-    static int Fibonacci(int n)
-    {
-        Console.WriteLine("The output is: ");
-        int n1 = 0;
-        int n2 = 1;
-        int sum;
-
-        for (int i = 2; i < n; i++)
+        static void Main(string[] args)
         {
-            sum = n1 + n2;
-            n1 = n2;
-            n2 = sum;
+            Console.WriteLine("Welcome to Galaxy News!");
+            IterateThroughList();
+            Console.ReadKey();
         }
+        /// <summary>
+        /// Выводит в консоль список галактик с названием, расстоянием и типом
+        /// </summary>
+        private static void IterateThroughList()
+        {
+            var theGalaxies = new List<Galaxy>
+        {
+            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+        };
 
-        return n == 0 ? n1 : n2;
+            foreach (Galaxy theGalaxy in theGalaxies)
+            {
+                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType.MyGType);
+            }
+
+            // Expected Output:
+            //  Tadpole  400,  Spiral
+            //  Pinwheel  25,  Spiral
+            //  Cartwheel, 500,  Lenticular
+            //  Small Magellanic Cloud .2,  Irregular
+            //  Andromeda  3,  Spiral
+            //  Maffei 1,  11,  Elliptical
+        }
+    }
+    /// <summary>
+    /// Класс, представляющий галактику
+    /// </summary>
+    public class Galaxy
+    {
+        public string Name { get; set; }
+
+        public double MegaLightYears { get; set; }
+        public GType GalaxyType { get; set; }
+
+    }
+    /// <summary>
+    /// Класс, определяющий тип галактики по символьному коду
+    /// </summary>
+    public class GType
+    {
+        public GType(char type)
+        {
+            switch (type)
+            {
+                case 'S':
+                    MyGType = Type.Spiral;
+                    break;
+                case 'E':
+                    MyGType = Type.Elliptical;
+                    break;
+                case 'I':
+                    MyGType = Type.Irregular;
+                    break;
+                case 'L':
+                    MyGType = Type.Lenticular;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public object MyGType { get; set; }
+        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
     }
 }
